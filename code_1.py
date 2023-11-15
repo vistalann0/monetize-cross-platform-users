bc4d09582b8695a82311794412062a5c67905a95
vistalann0 <146479343+vistalann0@users.noreply.github.com>
2023-11-15 09:41:15 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,13 +1,14 @@
-        }
-    }
-    public static void main(String[] args) {
+def main():
 
-        }
-public class RandomDataGenerator {
+def generate_random_data():
+import random
+    main()
 
-            data[i] = (int) (Math.random() * 100) + 1;
-        int[] data = new int[10];
-        for (int item : data) {
-            System.out.println("Random Number: " + item);
-        for (int i = 0; i < 10; i++) {
-}
+    for item in data:
+if __name__ == "__main__":
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+    return data
