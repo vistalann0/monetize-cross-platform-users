528b060dd7554a6878de4dc171c73ad4ea34ef3a
vistalann0 <146479343+vistalann0@users.noreply.github.com>
2023-11-15 08:12:48 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,13 @@
+        }
+    }
+    public static void main(String[] args) {
+
+        }
+public class RandomDataGenerator {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+}
