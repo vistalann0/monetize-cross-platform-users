738b056cdf620f4db948744bb0d0cbdb0fbddae1
vistalann0 <146479343+vistalann0@users.noreply.github.com>
2023-11-15 09:19:06 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,14 +1,14 @@
     for item in data:
 
 import random
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
 if __name__ == "__main__":
-
-def generate_random_data():
-
-
+        print(f"Random Number: {item}")
     data = generate_random_data()
     return data
-    main()
 def main():
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    main()
+def generate_random_data():
