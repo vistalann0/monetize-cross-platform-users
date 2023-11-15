cbb7f55e247ea49ced2166231068bca159512d05
vistalann0 <146479343+vistalann0@users.noreply.github.com>
2023-11-15 11:53:39 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,26 +1,40 @@
-    return data
-
-def generate_random_data():
-    for item in data:
-
-def main():
-        print(f"Random Number: {item}")
-    main()
-
-    data = generate_random_data()
     data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
-import random
-def main():
-
-    return data
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-def generate_random_data():
-    for item in data:
-
-if __name__ == "__main__":
         print(f"Random Number: {item}")
 
+
+    main()
+def generate_random_data():
+
+if __name__ == "__main__":
 import random
+
+    for item in data:
+    data = generate_random_data()
+    return data
+def main():import random
+
+
+        print(f"Random Number: {item}")
+
+    return data
+    for item in data:
+if __name__ == "__main__":
+
+def generate_random_data():
+    data = generate_random_data()
+def main():
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+def generate_random_data():
+import random
+
+    return data
+
+        print(f"Random Number: {item}")
+
+
+    for item in data:
+    main()
+    data = generate_random_data()
+def main():
